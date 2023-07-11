e3c17b6dafec023c595793561a96c6a31c366fa6
Tymofii-Shtyrba <timofeyshtyrba@gmail.com>
2023-07-11 04:03:11 +0300

    wrote the logic of requests

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,75 @@
 const express = require('express')
-
+const operations = require('../../models/contacts')
+const schema = require('../../validator');
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await operations.listContacts();
+    res.json(contacts);
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  try {
+    const contact = await operations.getContactById(req.params.id);
+
+    if (contact === undefined) {
+      throw { status: 404, message: 'Not found' };
+    }
+    res.json(contact);
+    
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = await schema.validate(req.body);
+
+    if (error) {
+      throw { status: 400, message: 'missing required name field' };
+    }
+    const contact = await operations.addContact(req.body);
+    res.status(201).json(contact);
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const deletedContact = await operations.removeContact(req.params.id);
+    if (!deletedContact) {
+      throw {status: 404, message: 'Not found' }
+    }
+    res.json({ message: 'contact deleted' });
+  } catch (error) {
+    next(error);
+  }
 })
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { error } = await schema.validate(req.body);
+
+		if (error) {
+			throw { status: 400, message: 'missing required name field' };
+    }
+    
+    const updatedContact = await operations.updateContact(req.params.id, req.body);
+
+    if (!updatedContact) {
+      throw { status: 404, message: 'Not found' };
+    }
+
+    res.json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
 })
 
 module.exports = router
---
+++ validator.js
@@ -0,0 +1,8 @@
+const Joi = require('joi');
+
+module.exports = new Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+})
+
